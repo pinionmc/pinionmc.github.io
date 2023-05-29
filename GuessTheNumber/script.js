401ff3143ddfc29ef26cdd97b8f6d3ae8b33b724
pinionmc <113288761+pinionmc@users.noreply.github.com>
2023-05-29 20:48:05 +0600

    just a little change

--- GuessTheNumber/script.js
+++ GuessTheNumber/script.js
@@ -7,37 +7,35 @@ let x = Math.floor(Math.random() * 100);
 const n = document.createElement("p");
 let Tries = 10;
 body.appendChild(n);
-n.style.color = "#0653b8"
-left.textContent = "Attempts left: " + Tries
+n.style.color = "#0653b8";
+left.textContent = "Attempts left: " + Tries;
 
 function get(event) {
   event.preventDefault();
-  Tries -= 1
+  Tries -= 1;
 
   if (x == input.value && Tries > 0) {
     all.remove();
-    
+
     n.textContent = "Congratulations! You guessed the correct number.";
-   
+
     setTimeout(() => {
       n.textContent = "Wanna try again?";
-    },1500)
+    }, 1500);
     setTimeout(() => {
-      location.reload()
-    },2500)
-  }
-  else if(Tries > -1 && x != input.value) {
-    input.value = ""
-    left.textContent = "Attempts left: " + Tries
-  }
-  else if(Tries < 0 && x != input.value) {
+      location.reload();
+    }, 2500);
+  } else if (Tries > -1 && x != input.value) {
+    input.value = "";
+    left.textContent = "Attempts left: " + Tries;
+  } else if (Tries < 0 && x != input.value) {
     all.remove();
-    n.textContent = "Alase! You have no attempts left!"
+    n.textContent = "Alase! You have no attempts left!";
     setTimeout(() => {
       n.textContent = "Wanna try again?";
-    },1500)
+    }, 1500);
     setTimeout(() => {
-      location.reload()
-    },2500)
+      location.reload();
+    }, 2500);
   }
-  }
+}

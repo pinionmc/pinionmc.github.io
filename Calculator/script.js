ea78c64c4e95b8fb5554a17ca52852fdbb495aaf
pinionmc <113288761+pinionmc@users.noreply.github.com>
2023-05-30 22:41:13 +0600

    Removed terporary home page

---
+++ Calculator/script.js
@@ -0,0 +1,59 @@
+const body = document.getElementById("body");
+const screen = document.getElementById("screen");
+const buttons = {};
+for (let i = 0; i <= 9; i++) {buttons["b" + i] = document.getElementById("b" + i);}
+const be = document.getElementById("be");
+const bd = document.getElementById("bd");
+const bD = document.getElementById("bD");
+const bs = document.getElementById("bs");
+const bm = document.getElementById("bm");
+const ba = document.getElementById("ba");
+
+function bu1() {
+  alert("ok");
+}
+function bu2() {
+  alert("ok");
+}
+function bu3() {
+  alert("ok");
+}
+function bu4() {
+  alert("ok");
+}
+function bu5() {
+  alert("ok");
+}
+function bu6() {
+  alert("ok");
+}
+function bu7() {
+  alert("ok");
+}
+function bu8() {
+  alert("ok");
+}
+function bu9() {
+  alert("ok");
+}
+function bu0() {
+  alert("ok");
+}
+function bud() {
+  alert("ok");
+}
+function buD() {
+  alert("ok");
+}
+function bus() {
+  alert("ok");
+}
+function bua() {
+  alert("ok");
+}
+function bum() {
+  alert("ok");
+}
+function bue() {
+  alert("ok");
+}
